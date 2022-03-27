b201a5b59ff1c0a8443d23b286fd9d3e67168a85
grom311 <grom311@yandex.ru>
2022-03-27 22:35:56 +0300

    add new post

--- movie_service/app/api/db.py
+++ movie_service/app/api/db.py
@@ -1,11 +1,14 @@
 # app/api/db.py
+DATABASE_URL = 'postgresql://postgres:12345@localhost/movie_db'
 
-from sqlalchemy import (Column, Integer, MetaData, String, Table,
+import os
+
+from sqlalchemy import (Column, DateTime, Integer, MetaData, String, Table,
                         create_engine, ARRAY)
 
 from databases import Database
 
-DATABASE_URL = 'postgresql://postgres:12345@localhost/movie_db'
+# DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
@@ -17,7 +20,7 @@ movies = Table(
     Column('name', String(50)),
     Column('plot', String(250)),
     Column('genres', ARRAY(String)),
-    Column('casts', ARRAY(String))
+    Column('casts_id', ARRAY(Integer))
 )
 
 database = Database(DATABASE_URL)
